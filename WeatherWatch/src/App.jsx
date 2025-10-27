2627c796314da65ce984047ff9c6e4797f50be6d
Hailey <hailey.gonzalez@slu.edu>
2025-10-27 16:18:19 -0500

    working react environment

--- WeatherWatch/src/App.jsx
+++ WeatherWatch/src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Weather Watch</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+       {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Trip your plan today!
         </p>
       </div>
       <p className="read-the-docs">
