b9927650926868e07ef21e0bc84123a0774d55c6
Hailey <hailey.gonzalez@slu.edu>
2025-10-28 12:42:14 -0500

    debugging main home screen

--- WeatherWatch/src/App.jsx
+++ WeatherWatch/src/App.jsx
@@ -9,7 +9,6 @@ import About from './About';
 
 function App() {
   return (
-
     <>
       <Navbar />
       <main>
@@ -20,28 +19,6 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Weather Watch</h1>
-      <div className="card">
-       {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <p>
-          Trip your plan today!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
     </>
   );
 }
