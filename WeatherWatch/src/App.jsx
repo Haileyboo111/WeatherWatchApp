3b0e19a40a3431bc3c1a0777add2da884395808b
Hailey <hailey.gonzalez@slu.edu>
2025-11-04 13:32:59 -0600

    new Branch to fix login debugging

--- WeatherWatch/src/App.jsx
+++ WeatherWatch/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
