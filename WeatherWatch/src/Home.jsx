8a7e45b3f5bfd3edd35508a65b52f7c7f9497f7e
Clarisa-A <clarisagu45@gmail.com>
2025-11-21 21:56:15 -0600

    added logo to front page
    Added a logo but feel free to change it to whatever you'd like!

--- WeatherWatch/src/Home.jsx
+++ WeatherWatch/src/Home.jsx
@@ -1,19 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import rainyDay from './assets/rainy-day_3750491.png'
 
 function Home() {
       return (
     <>
-        <h1>Welcome to Weather Watch!</h1>;
+        <h1>Welcome to Weather Watch!</h1>
 
         <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+          <img src={rainyDay} className="logo" alt="Rainy day logo" />
+        </div>
       <div className="card">
         <p>
           Plan Smart. Travel Safe.
@@ -24,4 +18,3 @@ function Home() {
 }
 
 export default Home;
-
