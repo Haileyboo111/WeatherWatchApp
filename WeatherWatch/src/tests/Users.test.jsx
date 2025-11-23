9fbd82ad2ccd23b575c733e7337584e449b15bd2
Hailey <hailey.gonzalez@slu.edu>
2025-11-23 11:59:41 -0600

    fixing tests and About text CSS

--- WeatherWatch/src/tests/Users.test.jsx
+++ WeatherWatch/src/tests/Users.test.jsx
@@ -5,7 +5,7 @@ import Users from "../Users";
 import * as AuthModule from "../context/AuthContext";
 import { vi } from "vitest";
 
-describe("Users Component", () => {
+describe("Users Component - Login", () => {
 
     beforeEach(() => {
         vi.spyOn(AuthModule, "useAuth").mockReturnValue({
@@ -45,3 +45,53 @@ describe("Users Component", () => {
         expect(passwordInput.value).toBe("password1234");
     });
 });
+
+describe("Users Component - Registration", () => {
+
+    beforeEach(() => {
+        vi.spyOn(AuthModule, "useAuth").mockReturnValue({
+            login: vi.fn(),
+            user: null,
+        });
+    });
+
+
+    const renderWithProviders = () =>
+        render(
+        <MemoryRouter>
+            <Users />
+        </MemoryRouter>
+    );
+
+    test("renders registration title when mode switched", () => {
+        renderWithProviders();
+        fireEvent.click(screen.getByText(/Register/i));
+        expect(screen.getByRole("heading", { name: /Register/i })).toBeInTheDocument();
+    });
+
+    test("shows error if name, email, or password are empty", () => {
+        renderWithProviders();
+        fireEvent.click(screen.getByText(/Register/i));
+        fireEvent.click(screen.getByRole("button", { name: /Register/i }));
+        expect(screen.getByRole("alert")).toHaveTextContent(/enter name, email, and password/i);
+    });
+
+    test("correctly updates name, email, and password inputs", () => {
+        renderWithProviders();
+        fireEvent.click(screen.getByText(/Register/i));
+
+        const nameInput = screen.getByPlaceholderText(/enter name/i);
+        const emailInput = screen.getByPlaceholderText(/enter email/i);
+        const passwordInput = screen.getByPlaceholderText(/enter password/i);
+
+        fireEvent.change(nameInput, { target: { value: "Test"}})
+        fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+        fireEvent.change(passwordInput, { target: { value: "passwordTest1234" } });
+
+        expect(nameInput.value).toBe("Test");
+        expect(emailInput.value).toBe("test@example.com");
+        expect(passwordInput.value).toBe("passwordTest1234");
+    });
+});
+
+//More tests?
